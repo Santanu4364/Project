bf87209a6c02c1a1dc442e869a540e537505b820
Santanu4364 <santanumandal841@gmail.com>
2026-02-18 14:36:50 +0530

    Initial commit

--- birthday-wisher/vite.config.js
+++ birthday-wisher/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/birthday-wisher/',
-})
+  // Only use the subfolder path for production (GitHub Pages), use root '/' for dev
+  base: mode === 'production' ? '/birthday-wisher/' : '/',
+}))
